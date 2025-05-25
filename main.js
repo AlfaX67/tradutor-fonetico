33088330342ec8030abf3006a9459ee41ad36412
AlfaX67 <alexander.pegorare@ibge.gov.br>
2025-05-25 19:45:30 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const app = document.getElementById("root");
+app.innerHTML = `
+  <h1>Tradutor Fonético</h1>
+  <p>Interface de teste para YouTube com legenda fonética + tradução.</p>
+  <p>O player real com legendas será incluído na versão final.</p>
+`;
